f742523dde9cea4c80a463c6bf827b7be79e3110
AlessandroFinocchi <alessandro.22082001@gmail.com>
2024-09-08 19:39:07 +0200

    volumes test

--- main.go
+++ main.go
@@ -1,52 +1,63 @@
 package main
 
 import (
-	"context"
-	"github.com/AlessandroFinocchi/sdcc_common/pb"
-	m "sdcc_host/model"
-	s "sdcc_host/services"
-	"sdcc_host/vivaldi"
+	"fmt"
+	"os"
 )
 
 func main() {
-	ctx := context.Background()
-
-	// Initialize Protocols
-	rc, uniqueId := s.NewRegistryConnectorClient()
-	filter := vivaldi.NewFilter()
-	membershipProtocol := s.NewMembershipProtocol(filter)
-	vivaldiGossip := s.NewVivaldiGossip(filter)
-	vivaldiProtocol := s.NewVivaldiProtocol(vivaldiGossip, filter)
-
-	// Start Protocols and get address infos
-	membershipServerIp, membershipServerPort := membershipProtocol.StartServer()
-	vivaldiServerIp, vivaldiServerPort := vivaldiProtocol.StartServer()
-	gossipServerip, gossipServerPort := vivaldiGossip.StartServer()
-
-	// Init current server node
-	currentServerNode := &pb.Node{
-		Id:             uniqueId,
-		MembershipIp:   membershipServerIp,
-		MembershipPort: membershipServerPort,
-		VivaldiIp:      vivaldiServerIp,
-		VivaldiPort:    vivaldiServerPort,
-		GossipIp:       gossipServerip,
-		GossipPort:     gossipServerPort,
+	file, err := os.Open("results.txt") // Write the file to /data (mapped to a volume)
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+	_, err = file.WriteString("Hello, Docker!\n")
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
 	}
 
-	// Connect to Registry
-	startingNodeList := rc.Connect(ctx, currentServerNode)
-
-	// Init partial view
-	pView := m.NewPartialView(currentServerNode, startingNodeList)
-	membershipProtocol.SetPartialView(pView)
-	vivaldiProtocol.SetPartialView(pView)
-	vivaldiGossip.SetPartialView(pView)
-
-	// Start client protocols
-	go membershipProtocol.StartClient()
-	go vivaldiProtocol.StartClient()
-	go vivaldiGossip.StartClient()
-
-	select {}
+	fmt.Println("Successfully wrote to output.txt")
+	return
+	//ctx := context.Background()
+	//
+	//// Initialize Protocols
+	//rc, uniqueId := s.NewRegistryConnectorClient()
+	//filter := vivaldi.NewFilter()
+	//membershipProtocol := s.NewMembershipProtocol(filter)
+	//vivaldiGossip := s.NewVivaldiGossip(filter)
+	//vivaldiProtocol := s.NewVivaldiProtocol(vivaldiGossip, filter)
+	//
+	//// Start Protocols and get address infos
+	//membershipServerIp, membershipServerPort := membershipProtocol.StartServer()
+	//vivaldiServerIp, vivaldiServerPort := vivaldiProtocol.StartServer()
+	//gossipServerip, gossipServerPort := vivaldiGossip.StartServer()
+	//
+	//// Init current server node
+	//currentServerNode := &pb.Node{
+	//	Id:             uniqueId,
+	//	MembershipIp:   membershipServerIp,
+	//	MembershipPort: membershipServerPort,
+	//	VivaldiIp:      vivaldiServerIp,
+	//	VivaldiPort:    vivaldiServerPort,
+	//	GossipIp:       gossipServerip,
+	//	GossipPort:     gossipServerPort,
+	//}
+	//
+	//// Connect to Registry
+	//startingNodeList := rc.Connect(ctx, currentServerNode)
+	//
+	//// Init partial view
+	//pView := m.NewPartialView(currentServerNode, startingNodeList)
+	//membershipProtocol.SetPartialView(pView)
+	//vivaldiProtocol.SetPartialView(pView)
+	//vivaldiGossip.SetPartialView(pView)
+	//
+	//// Start client protocols
+	//go membershipProtocol.StartClient()
+	//go vivaldiProtocol.StartClient()
+	//go vivaldiGossip.StartClient()
+	//
+	//select {}
 }
